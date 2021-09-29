1efb298d0621563c93afa2dee0a6989ecdb57e90
Niroh <mamfred.fajardo@gmail.com>
2021-09-29 23:31:43 +0200

    Prepared to start with input.js

--- snake.js
+++ snake.js
@@ -1,13 +1,13 @@
 export const SNAKE_SPEED = 1;
 const snakeBody = [{ x:11, y:11 }];
 
-export function update()       {
+export function update() {
     for(let i = snakeBody.length - 2; 1 >=0; i--){
         snakeBody[i + 1] = { ...snakeBody[i] }
     }
 
-    snakeBody[0].x += 1;
-    snakeBody[0].y += 0;
+    //snakeBody[0].x += 1;
+    //snakeBody[0].y += 0;
 }
 
 export function draw(gameBoard) {

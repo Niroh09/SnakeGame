0206ba83c79a7484e0375b927ca9dc6f6b041848
Niroh <mamfred.fajardo@gmail.com>
2021-09-30 23:34:20 +0200

    Creation and fix bug of food RandomGridPosition

--- food.js
+++ food.js
@@ -1,12 +1,14 @@
 import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 
-let food = getRandomFoodPosition
+
+let food =  getRandomFoodPosition()
 const EXPANSION_RATE = 1
 
 export function update() {
     if(onSnake(food)){
         expandSnake(EXPANSION_RATE)
-        food = { x: 20, y: 10 }
+        food = randomGridPosition()
     }
 }
 
